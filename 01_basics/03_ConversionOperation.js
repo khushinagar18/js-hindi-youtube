13325089b4e0078ce8a3d169219dc4cf438d0ce0
Khushi Nagar <115608185+khushinagar18@users.noreply.github.com>
2024-04-13 05:19:19 +0000

    new changes

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -23,8 +23,26 @@
 // "33abs" ==> NaN
 //  true ==> 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
+
+
+// ******************** Operations ********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "Hello"
+// let str2 = "Khushi"
+// let str3 = str1+str2
+// console.log(str3)
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2 + 2)
+console.log(1+2+"2")
