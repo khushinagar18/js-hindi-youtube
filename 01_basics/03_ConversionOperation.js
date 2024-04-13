a861204097e560c97a6a169fdff96300c3a3baf7
Khushi Nagar <115608185+khushinagar18@users.noreply.github.com>
2024-04-13 04:45:50 +0000

    Cahnges

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,9 +1,30 @@
 // let score = 33
-let score = "33abc"
+// let score = "33abc"
+// let score = null;
+// let score = undefined
+// let score = true
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)      //NaN
+
+// console.log(valueInNumber)         //0
+
+// console.log(valueInNumber)          //NaN
+
+// console.log(valueInNumber)     //1
+
+
+
+// "33" ==> 33
+// "33abs" ==> NaN
+//  true ==> 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
