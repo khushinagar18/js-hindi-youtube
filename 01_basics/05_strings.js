571e6a63733835f12a0e18a2d1ca8b7d3fde9c4a
Khushi Nagar <115608185+khushinagar18@users.noreply.github.com>
2024-04-14 14:28:21 +0000

    new file

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "khushi"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value") not recommended
+
+
+// **** String Interpolation**** ----------here we create placeholders and inject values
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`,)
+
+
+const gameName = new String('Khushi')  //String is a Object --Key value Pair
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
