18caa6bbaf4624a2108a4e8af73e46a5f239d1f5
Khushi Nagar <115608185+khushinagar18@users.noreply.github.com>
2024-04-16 04:21:41 +0000

    some

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -36,7 +36,7 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10
 const max = 20
 
-try to understand random imp
+// try to understand random imp
 console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
 
 
